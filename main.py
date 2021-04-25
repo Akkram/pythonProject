030060adda0434f341ed5c04b00e8aad43835250
ajebali <jebaliakram@gmail.com>
2021-04-25 18:05:27 +0200

    Strings in Python

--- main.py
+++ main.py
@@ -3,3 +3,15 @@ print(1 + 2)
 print(7 + 6)
 print()
 print("The end", "or is it ?", "keep watching to learn more about Python", 3)
+
+print("Today is a good day to learn Python")
+print ("Python is fun")
+print("Python's string are easy to use")
+print('We can even include "quotes" in strings')
+print("Hello" + " World")
+greeting="Hello"
+name=input("Please enter your name ")
+print(greeting+' '+name)
+print('Number\t1\t\tThe Larch')
+print('Number\t2\t\tThe Horse Chestnut')
+
